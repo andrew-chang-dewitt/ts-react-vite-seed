f82dd669809e3bcae26cbef2b5af72b0ec2dd1d4
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2024-04-10 15:33:03 -0500

    Improve assertions for testing w/ jest-dom.

--- src/Button.spec.tsx
+++ src/Button.spec.tsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
 import { render, screen } from "@testing-library/react";
-import { userEvent } from "@testing-library/user-event";
-import { expect, test } from "vitest";
+// import { userEvent } from "@testing-library/user-event";
+import { describe, expect, test } from "vitest";
 
-import { Button } from "./Button";
-import App from "./App";
+import { Button } from "$/Button";
 
-test("button starts with count of zero", async () => {
-  render(<App />);
+describe("button component", () => {
+  function setup(initialState: number): HTMLButtonElement {
+    function Test() {
+      let [count, setCount] = useState(initialState);
 
-  await userEvent.click(screen.getByRole("button"));
+      return (
+        <Button count={count} onClick={() => setCount((count) => count + 1)} />
+      );
+    }
 
-  expect(screen.getByRole("button").textContent).toBe("count is 0");
+    render(<Test />);
+
+    // await userEvent.click(screen.getByRole("button"));
+    return screen.getByRole("button");
+  }
+
+  test("starts with given initial count", async () => {
+    const button = setup(0);
+    expect(button).toHaveTextContent("count is 0");
+  });
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   test: {
     environment: "jsdom",
+    setupFiles: "./setupTest.ts",
   },
 });
